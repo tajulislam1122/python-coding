dcc2dd24aa4d8a68a5be92dba60cdcf596fd74d4
Taj ul islam <tajulswati2003@gmail.com>
2025-03-04 02:39:01 -0800

    Add files via upload

---
+++ if_condition.py
@@ -0,0 +1,92 @@
+# this is conditiononal statements 
+
+# # this program about the senair not senair 
+# age=int(input("enter your age ?"))
+# if age >= 20:
+#     print("senair !")
+# else:
+#     print("not senair !")
+
+# # pasitive and negative 
+# num = int(input("enter your number ?"))
+# if num>=0:
+#     print("it is positive !")
+# else:
+#     print("it is negative !")
+
+# this program about the if ,elif , else,
+# # marks and gread !
+# marks=int(input("enter your marks ?"))
+# if marks >= 90:
+#     print("A")
+# elif marks >= 80:
+#     print("B")
+# elif marks >= 70:
+#     print("C")
+# elif marks >= 60:
+#     print("D")
+# elif marks >= 50:
+#     print("pass")
+# else:
+#     print("fail !")
+
+# # positive and negative and zero 
+# num=int(input("enter your number ?"))
+# if num>0:
+#     print("Positive number !")
+# elif num==0:
+#     print("zero !")
+# else:
+#     print("negative ")
+
+# nested if program 
+# # this program about the age and license .
+# age =int(input("enter your age ?"))
+# has_license=True
+# if age > 20:
+#     if has_license:
+#         print("can drive !")
+#     else:
+#         print("Need license")
+# else:
+#     print("you'r too young !")
+
+# # greater than 10 and even 
+# num = int(input("enter your number ?"))
+# if num > 10 and num%2==0:
+#     print(f"{num} is greater than 10 and even !")
+# else:
+#     print(f"{num} don't greater than 10 and even !")
+
+# # check the number mul by 2 
+# num = int(input("enter your number ?"))
+# if num % 2 ==0:
+#     print("it is even !")
+# else:
+#     print("it is odd number !")
+
+# # fine the greatest of three number 
+# num1=int(input("enter your number1 ?"))
+# num2=int(input("enter your number2 ?"))
+# num3=int(input("enter your number3 ?"))
+
+# if num1>num2 and num1>num3:
+#     print(f"{num1} num1 is greater than num2 and num3!")
+# if num2>num1 and num2>num3:
+#     print(f"{num2} num2 is greater than num1 and num3 !")
+# if num3>num2 and num3>num1:
+#     print(f"num3 is greater than num2 and num1 !")
+
+# # check the number mul by 7 or not !
+# num = float(input("enter your number ?"))
+# if num % 7==0:
+#     print("yes !")
+# else:
+#     print("no !")
+
+# project about calculater 
+
+num1=("enter your num1 ?")
+num2=("enter your num2 ?")
+if not(num1==45*num2==3):
+    print(num1)
